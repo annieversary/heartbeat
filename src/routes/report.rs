90e08cff70b9c307da40bdab6fa6575502c67377
annieversary <annie@versary.town>
2023-12-12 04:39:34 +0000

    add filtering to /report

--- src/routes/report.rs
+++ src/routes/report.rs
@@ -1,35 +1,49 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use anyhow::Result;
-use axum::{extract::State, response::Html};
+use axum::{
+    extract::{Query, State},
+    response::Html,
+};
 use chrono::Duration;
 use maud::html;
 
 use crate::{errors::AppError, helpers::format_relative, html::base_template, AppState};
 
-pub async fn report(State(state): State<Arc<AppState>>) -> Result<Html<String>, AppError> {
+pub async fn report(
+    State(state): State<Arc<AppState>>,
+    Query(q): Query<HashMap<String, String>>,
+) -> Result<Html<String>, AppError> {
     struct Absence {
         start: String,
         end: String,
         length: String,
     }
 
-    let absences: Vec<Absence> = sqlx::query!("select * from absences order by id desc limit 1000")
-        .fetch_all(&state.pool)
-        .await?
-        .into_iter()
-        .map(|a| Absence {
-            end: a
-                .timestamp
-                .and_utc()
-                .format("%Y/%m/%d %H:%M UTC")
-                .to_string(),
-            start: (a.timestamp.and_utc() - Duration::seconds(a.duration))
-                .format("%Y/%m/%d %H:%M UTC")
-                .to_string(),
-            length: format_relative(a.duration),
-        })
-        .collect();
+    let duration = q
+        .get("duration")
+        .and_then(|s| s.parse::<u32>().ok())
+        .unwrap_or_default() as i64;
+
+    let absences: Vec<Absence> = sqlx::query!(
+        "select * from absences where duration > ? order by id desc limit 1000",
+        duration
+    )
+    .fetch_all(&state.pool)
+    .await?
+    .into_iter()
+    .map(|a| Absence {
+        end: a
+            .timestamp
+            .and_utc()
+            .format("%Y/%m/%d %H:%M UTC")
+            .to_string(),
+        start: (a.timestamp.and_utc() - Duration::seconds(a.duration))
+            .format("%Y/%m/%d %H:%M UTC")
+            .to_string(),
+        length: format_relative(a.duration),
+    })
+    .collect();
 
     let content = html! {
         ul {
